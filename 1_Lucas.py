ca468e3413f7f35bc8c36526c019e01bbcf708da
Falkzera <falcovisk@gmail.com>
2024-08-09 01:56:25 -0300

    Portifolio Falkzera

---
+++ 1_Lucas.py
@@ -0,0 +1,224 @@
+# Importações
+import streamlit as st
+from streamlit_option_menu import option_menu
+from PIL import Image
+import json
+import requests
+import re
+from map_visualization import create_map
+
+# Configuração de Página como wide
+st.set_page_config(layout="wide")
+
+# Função para carregar o arquivo JSON
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+# Função para validar email
+def is_valid_email(email):
+    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(regex, email) is not None
+
+# FUnção para rolar a página
+def scroll_to_projects():
+    st.write("""
+        <script>
+            document.getElementById('projetos-section').scrollIntoView({behavior: 'smooth'});
+        </script>
+    """, unsafe_allow_html=True)
+
+# Organização de Imagens e Lotties
+def load_assets():
+    assets = {}
+    assets['falkzera'] = Image.open("image/falkzera.png")
+    assets['python_snake'] = "image/python_snake.gif"
+    assets['mapa_al'] = "image/mapa_al.png"
+    assets['lottie_contato'] = load_lottiefile("image/Contact.json")
+    assets['foguete'] = load_lottiefile("image/foguete.json")
+    return assets
+
+# Carregar os ativos
+assets = load_assets()
+
+## Função para criar botões de redes sociais com ícones
+# Função para criar botões de redes sociais
+def social_media_button(platform, url):
+    if st.button(platform):
+        st.write(f"[Abrir {platform}]({url})")
+
+#####################################################################################################
+# SideBar Config
+
+with st.sidebar:
+    social_media_html = """
+    <div style="text-align: center;">
+        <h2>Redes Sociais</h2>
+        <a href="https://www.instagram.com/falkzera/" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram" style="width:40px;height:40px;margin:10px;">
+        </a>
+        <a href="https://github.com/falkzera" target="_blank">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" style="width:40px;height:40px;margin:10px;">
+        </a>
+    </div>
+    """
+    st.markdown(social_media_html, unsafe_allow_html=True)
+
+    st.markdown('---')
+    # st.lottie(assets['foguete'], height=200, speed=10)
+    st.sidebar.markdown('Developer by: [Lucas Falcão](https://GitHub.com/Falkzera)')
+
+
+# Container Principal
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("##")
+        st.subheader("Seja bem-vindo ao meu perfil! :wave: ")
+        st.title("Profile: Lucas Falcão 🦅")
+        st.caption("23 anos, Maceió-AL")
+        texto = """
+        Olá, sou Lucas Falcão, graduando de Ciências Econômicas pela Universidade Federal de Alagoas - UFAL, amante de Python, Data Science e Econometria.
+        Aqui você encontrará um pouco sobre mim, meus projetos e como entrar em contato. 🚀
+
+        Comecei a estudar programação, em especial: python 🐍, no meu quinto período da faculdade, e desde então venho me aprimorando e estudando cada vez mais.
+        Então, tudo que você verá aqui é fruto de muito estudo e dedicação ❤️
+
+        Siga os menus abaixo, e visualize as informações que deseja. Caso queira entrar em contato, utilize o formulário no menu "Contato":email: logo abaixo.
+        Ou se preferir, entre em contato pelas redes sociais.
+        """
+        # HTML para justificar o texto
+        html_text = f"""
+        <div style="text-align: justify;">
+            {texto}
+        </div>
+        """
+        # Exibir o texto justificado no Streamlit
+        st.markdown(html_text, unsafe_allow_html=True)
+        #     st.write("## Redes Sociais")
+
+    with col2:
+        st.image(assets['falkzera'], width=400, use_column_width=True)
+        
+
+# Menu de Navegação 
+with st.container():
+    selected = option_menu(
+        menu_title = None,
+        options = ["Sobre mim","Cursos", "Projetos", "Contato"],
+        icons = ['person', 'book', 'code-slash', 'chat-left-text-fill'],
+        orientation='horizontal',
+    )
+st.write("---")
+
+# Opções de Navegações
+# Sobre
+
+if selected == "Sobre mim":
+    scroll_to_projects()
+    pass
+
+
+if selected == 'Cursos':
+    
+        
+    col1, col2 = st.columns(2)
+    with col1: # FORMAÇÃO ACADÊMICA
+        with st.expander("**FORMAÇÃO E ESPECIALIZAÇÃO**", expanded=False):
+            st.write("## Graduação")
+            st.write("Ciências Econômicas - Universidade Federal de Alagoas - UFAL")
+            st.caption("Em andamento")
+            st.write("---")
+
+    
+    with col2: # CURSOS E CERTIFICADOS
+        with st.expander("**CURSOS COM CERTIFICADOS**", expanded=False):
+            st.write("## Cursos")
+            st.write("#### **Escola Nacional de Administração Pública (ENAP)**")
+            st.write("Introdução à Ciência de Dados - Conjuntos Frequentes - 2024")
+            st.write("Aprendendo com Python - 2024")
+            st.write("Estatística - 2024")
+            st.write("---")
+
+# Projetos
+if selected == "Projetos":
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander("**EXPORTAÇÃO ALAGOAS** 🌍", expanded=False):
+                st.write("## Análise das exportações no estado de Alagoas")
+                st.write("O objetivo desse projeto foi de analisar as exportações do estado de Alagoas, com base nos dados disponibilizados pelo Governo Federal. Realizando uma análise exploratória dos dados, e criando um mapa coroplético para visualização dos dados.")
+                st.write('Visualize o projeto completo: [Clique Aqui](https://exportacoes.streamlit.app/)')
+                st.caption("Fonte: Governo Federal")
+                create_map()
+    with col2:
+        with st.expander("**EDIFICAÇÕES AGROPECUÁRIAS** 🌍", expanded=False):
+                st.write("## Mapeamento das edificações agropecuárias no estado de Alagoas")
+                st.write("O intuito do projeto foi realizar um mapeamento das edificações agropecuárias no estado de Alagoas.")
+                st.write('Visualize o projeto completo: [Clique Aqui](https://exportacoes.streamlit.app/)')
+                st.caption("Fonte: IBGE")
+                
+ 
+
+
+
+
+
+
+
+
+# Contato
+# Formulário de Contato
+if selected == "Contato":
+    scroll_to_projects()
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("Envie-me um Email :email:")
+        st.caption("E-mail via FormSubmit. O aplicativo está em desenvolvimento, então pode não funcionar corretamente. Caso não funcione, entre em contato pelas redes na sidebar.")
+    with col2:
+        st.lottie(assets['lottie_contato'], height=100)
+
+# Validação para e-mail
+    with st.form(key='contact_form'):
+        name = st.text_input("Seu Nome", max_chars=50)
+        email = st.text_input("Seu Email", max_chars=50, type='default')
+        if email:
+            if not is_valid_email(email):
+                st.error("Por favor, insira um email válido.")
+        message = st.text_area("Sua Mensagem", height=150)
+        submit_button = st.form_submit_button(label='Enviar')
+
+    if submit_button:
+        if name and email and message:
+            # Lógica para enviar o email usando FormSubmit
+            form_data = {
+                'name': name,
+                'email': email,
+                'message': message
+            }
+            response = requests.post(
+                'https://formsubmit.co/falcovisk@gmail.com',
+                data=form_data
+            )
+            if response.status_code == 200:
+                st.warning("Algo de estranho ocorreu!")
+            else:
+                st.error("Falha ao enviar a mensagem. Tente novamente mais tarde.")
+        else:
+            st.error("Por favor, preencha todos os campos.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ map_visualization.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import json
+
+# Função para carregar dados e criar o mapa
+def create_map():
+    # Base de dados
+    df = pd.read_csv('Portifolios/exportacoes/SerieExportacoesAL.csv')
+
+    # Geojson com os municípios de Alagoas
+    with open('Portifolios/exportacoes/Alagoas_Mapa.geojson') as f:
+        geojson = json.load(f)
+
+    # Lista de municípiosss
+    municipios_geojson = [feature['properties']['NM_MUN'] for feature in geojson['features']]
+
+    # Temas disponíveis
+    temas = ['Blues', 'Magma']
+
+    # Filtro de Tema
+    tema_selecionado = st.selectbox('Selecione o tema do mapa:', temas)
+
+    # Filtro de Data
+    ano_inicial = df['CO_ANO'].min()
+    anos_disponiveis = df['CO_ANO'].unique()
+    ano_final = st.slider('Selecione a data final:', min_value=ano_inicial, max_value=anos_disponiveis.max(), value=ano_inicial)
+
+    # Filtrar os dados com base no intervalo de anos selecionado
+    df_filtrado = df[(df['CO_ANO'] >= ano_inicial) & (df['CO_ANO'] <= ano_final)]
+
+    # Agrupar os dados por município e somar os valores
+    df_filtrado = df_filtrado.groupby('Nome_Município').sum().reset_index()
+
+    # Criar um DataFrame completo com todos os municípios e valores zerados
+    df_completo = pd.DataFrame(municipios_geojson, columns=['Nome_Município'])
+    df_completo = df_completo.merge(df_filtrado, on='Nome_Município', how='left').fillna(0)
+
+    # Definir o valor mínimo fixo em 0
+    valor_minimo = 0
+
+    # Slider para o valor máximo com legenda "Valor em US$"
+    valor_maximo = st.slider(
+        'Valor em US$:',
+        min_value=float(df_completo['VL_FOB'].min()),  
+        max_value=float(df_completo['VL_FOB'].max()) + 1_000_000,  
+        value=float(df_completo['VL_FOB'].mean()) + 1_000_000,  
+        format="$%.0f"  # Formato dos valores do slider
+    )
+
+    # Função para criar o mapa coroplético
+    def make_choropleth(input_df, geojson, input_id, input_column, input_color_theme, valor_minimo, valor_maximo):
+        choropleth = px.choropleth(
+            input_df,
+            geojson=geojson,
+            locations=input_id,
+            featureidkey="properties.NM_MUN",
+            color=input_column,
+            color_continuous_scale=input_color_theme,
+            range_color=(valor_minimo, valor_maximo),
+            scope="south america",
+            labels={input_column: 'Valor'}
+        )
+        choropleth.update_geos(fitbounds="locations", visible=False)
+        choropleth.update_layout(
+            plot_bgcolor='rgba(0, 0, 0, 0)',
+            paper_bgcolor='rgba(0, 0, 0, 0)',
+            margin=dict(l=0, r=0, t=0, b=0),
+            height=400,
+            coloraxis_showscale=False,
+            geo=dict(bgcolor='rgba(0,0,0,0)')
+        )
+        return choropleth
+
+    # Parâmetros para a função
+    input_id = 'Nome_Município'  
+    input_column = 'VL_FOB'   
+
+    # Mapa coroplético
+    choropleth_fig = make_choropleth(df_completo, geojson, input_id, input_column, tema_selecionado, valor_minimo, valor_maximo)
+
+    # Mostrar o mapa
+    st.plotly_chart(choropleth_fig)
+
+# Chamar a função create_map
+create_map()
